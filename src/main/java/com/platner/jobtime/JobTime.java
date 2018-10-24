34aa0868b912a7e33969c3a651abdcff9ac970b7
cplatner <chris@platner.com>
2018-10-24 11:56:17 -0700

    - Rename application

---
+++ src/main/java/com/platner/jobtime/JobTime.java
@@ -0,0 +1,13 @@
+package com.platner.jobtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTime
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JobTime.class);
+    }
+}
